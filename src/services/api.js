d3ff3bc022a3cfb6142f479af3c1b04b6ba7b9c1
Wallace Santana <wallacesantanak0@gmail.com>
2024-08-08 17:14:45 -0300

    first deploy

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const Api = axios.create({baseURL: "http://192.168.248.200:3001"})
+const Api = axios.create({baseURL: process.env.REACT_APP_BASE_API});
 
 export default Api;
