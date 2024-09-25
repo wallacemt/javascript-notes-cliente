9d3cd083e4abb4491cb323d199f803bd15f578e2
Wallace Santana <wallacesantanak0@gmail.com>
2024-09-25 17:45:24 -0300

    editor edit

--- src/components/notes/editor/index.jsx
+++ src/components/notes/editor/index.jsx
@@ -24,25 +24,20 @@ function Editor(props) {
         <Fragment>
             <TinyMCEEditor
                 value={currentContent}
-                apiKey={'bnkcvpnss1lbevivp007qsax2o5ynydm9u5ml4do28bn2g4d'}
+                apiKey="bnkcvpnss1lbevivp007qsax2o5ynydm9u5ml4do28bn2g4d"  
                 init={{
-                    height: '92vh',
-                    width: '100%',
-                    language: 'pt_BR',
+                    height: '100vh',  
+                    width: '100%',   
                     menubar: false,
-                    skin:"oxide-dark",
                     plugins: [
-                        'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
-                        'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown',
+                        'advlist autolink lists link image charmap print preview anchor',
+                        'searchreplace visualblocks code fullscreen',
+                        'insertdatetime media table paste code help wordcount'
                     ],
-                    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
-                    tinycomments_mode: 'embedded',
-                    tinycomments_author: 'Author name',
-                    mergetags_list: [
-                        { value: 'First.Name', title: 'First Name' },
-                        { value: 'Email', title: 'Email' },
-                    ],
-
+                    toolbar:
+                        'undo redo | formatselect | bold italic backcolor | ' +
+                        'alignleft aligncenter alignright alignjustify | ' +
+                        'bullist numlist outdent indent | removeformat | help'
                 }}
                 onEditorChange={handleEditorChange}
             />
