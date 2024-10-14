0a9eeef5b1e1561abc772f45140264ac6ab2ec83
Wallace Santana <138271229+wallacemt@users.noreply.github.com>
2024-10-14 10:34:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,6 @@ import AppRoutes from './routes';
 const App = () => {
   return (
     <Fragment>
-      <head> 
-           <meta name="trustpilot-one-time-domain-verification-id" content="4a89f9e5-46f8-4b3a-8e48-07cd7c0676d8"/>
-           <meta name="google-site-verification" content="sXuwBe0uCnkVMGMTNO9m5zh4DkbRGgA60VPq_U5ZXzg" />
-      </head>
       <AppRoutes/>
     </Fragment>
   );
