8cba3b0a8b066e49d3ae9fecb3dc8589033d73f1
Wallace Santana <138271229+wallacemt@users.noreply.github.com>
2024-10-14 09:51:59 -0300

    Update index.jsx

--- src/screens/home/index.jsx
+++ src/screens/home/index.jsx
@@ -7,10 +7,6 @@ import { Link } from "react-router-dom";
 const HomeScreen = () => {
     return (
         <Fragment>
-            <head> 
-                  <meta name="trustpilot-one-time-domain-verification-id" content="4a89f9e5-46f8-4b3a-8e48-07cd7c0676d8"/>
-                  <meta name="google-site-verification" content="sXuwBe0uCnkVMGMTNO9m5zh4DkbRGgA60VPq_U5ZXzg" />
-            </head>
             <Header />
             <section className="section home">
                
